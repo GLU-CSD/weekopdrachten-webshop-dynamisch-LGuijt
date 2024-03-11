3543eec2e325826046f1b6c008e84884dcfd2383
LGuijt <mail.lisageurts@gmail.com>
2024-03-11 16:05:36 +0100

    commit 1

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <div class="mainpagelinks"><!--links boven de producten-->
     <p><a href="/homepage.php">Home</a> > <a href="/index.php">Shop</a> > Schilderijen</p>
   </div>
-  <div class="sidebar">
+  <form action="" method="get" class="sidebar">
     <div class="filters"> <!--filters voor de maat, je kan er op klikken verder niet functioneel-->
       <p style="border-bottom: 1px solid black; font-size: 18px;">Maat</p>
       <p><input type="checkbox" name="maat" value="maatxs">XS</p>
@@ -12,20 +12,22 @@
       <p><input type="checkbox" name="maat" value="maatl">L</p>
       <p><input type="checkbox" name="maat" value="maatxl">XL</p>
     </div>
+
     <div class="filters"> <!--filters voor de kleur, je kan er op klikken verder niet functioneel-->
       <p style="border-bottom: 1px solid black; font-size: 18px;">Kleur</p>
-      <p><input type="checkbox" name="kleur" value="blauw">Blauw</p>
-      <p><input type="checkbox" name="kleur" value="geel">Geel</p>
-      <p><input type="checkbox" name="kleur" value="goud">Goud</p>
-      <p><input type="checkbox" name="kleur" value="groen">Groen</p>
-      <p><input type="checkbox" name="kleur" value="oranje">Oranje</p>
-      <p><input type="checkbox" name="kleur" value="paars">Paars</p>
-      <p><input type="checkbox" name="kleur" value="rood">Rood</p>
-      <p><input type="checkbox" name="kleur" value="roze">Roze</p>
-      <p><input type="checkbox" name="kleur" value="wit">Wit</p>
-      <p><input type="checkbox" name="kleur" value="zilver">Zilver</p>
-      <p><input type="checkbox" name="kleur" value="zwart">Zwart</p>
+      <p><input type="checkbox" name="kleur[]" value="blauw">Blauw </p>
+      <p><input type="checkbox" name="kleur[]" value="geel">Geel</p>
+      <p><input type="checkbox" name="kleur[]" value="goud">Goud</p>
+      <p><input type="checkbox" name="kleur[]" value="groen">Groen</p>
+      <p><input type="checkbox" name="kleur[]" value="oranje">Oranje</p>
+      <p><input type="checkbox" name="kleur[]" value="paars">Paars</p>
+      <p><input type="checkbox" name="kleur[]" value="rood">Rood</p>
+      <p><input type="checkbox" name="kleur[]" value="roze">Roze</p>
+      <p><input type="checkbox" name="kleur[]" value="wit">Wit</p>
+      <p><input type="checkbox" name="kleur[]" value="zilver">Zilver</p>
+      <p><input type="checkbox" name="kleur[]" value="zwart">Zwart</p>
     </div>
+
     <div class="filters"><!--prijs sliders-->
       <p style="border-bottom: 1px solid black; font-size: 18px;">Prijs</p>
       <p>Min. Prijs:</p>
@@ -47,8 +49,9 @@
         <input type="range" min="0" max="100" name="maxprice" id="maxprice" value="100">
       </div>
     </div>
-    <button class="toepassen">Toepassen</button> <!--knop voor de filters toepassen. klikbaar-->
-  </div>
+    <input type=submit class="toepassen" value="Toepassen"> <!--knop voor de filters toepassen. klikbaar-->
+  </form>
+
   <div id="products">
     <div id="producttop"><!--titel van de pagina, en de knop voor het sorteren-->
       <div id="title">
@@ -72,7 +75,8 @@
         $file = json_decode($file_json, true);
 
         foreach ($file as $x) {
-          if ($x["category"] == "Schilderijen") {
+          $filter = filefilter($x);
+          if ($x["category"] == "Schilderijen" && $filter == true) {
             echo '<div class="product">';
             echo '<div class="pic"><img class="' . $x["imgsize"] . '" src="' . $x["img"] . '" alt="schilderij"></div>';
             echo '<div class="productname">' . $x["name"] . '</div>';
@@ -88,6 +92,32 @@
             echo '</div>';
             echo '</div>';
             $productamount++;
+          }
+        }
+
+        function filefilter($prod)
+        {
+          if ($_SERVER["REQUEST_METHOD"] == "GET") {
+            $colorCheck = false;
+            if (isset($_GET['kleur'])) {
+              $klr = $_GET['kleur'];
+              if (empty($klr)) {
+                $colorCheck = true;
+              } else {
+                $colorCheck = false;
+              }
+            } else {
+              $colorCheck = true;
+            }
+
+            if ($colorCheck){
+              return true;
+            } else {
+              return false;
+            }
+
+          } else {
+            return true;
           }
         }
         ?>
